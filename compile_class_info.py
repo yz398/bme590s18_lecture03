1459827755166d53a9f59fe9c840ddb4d36f2b27
Mark Palmeri <mlp6@duke.edu>
2018-01-26 11:30:35 -0500

    add pseudocode for python data cat

---
+++ compile_class_info.py
@@ -0,0 +1,17 @@
+def main():
+    filenames = collect_csv_files()
+    student_data = cat_data()
+    write_data()
+
+def collect_csv_files():
+    pass
+
+def cat_data():
+    pass
+
+def write_():
+    # CSV or JSON
+    pass
+
+if __name__ == "__main__":
+    main()
